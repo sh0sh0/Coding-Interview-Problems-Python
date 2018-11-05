5e57f9a1e5871270f4ff527b0a9870d0cc6444c8
sh0sh0 <xue.yun.qi@gmail.com>
2018-11-05 14:11:47 -0800

    Create Even number first.py

---
+++ Even number first.py
@@ -0,0 +1,15 @@
+## given a list of integers, return the list with even numbers appear first
+## you can create a new list, which will require extra space
+## you can also using two pointers, one for even (starts from the beginning), and one for odd (starts from the end), to 
+## the list into three parts: even, unclassified, odd
+
+def evenFirst(A):
+  even, odd = 0, len(A) - 1
+  while even < odd:
+    if A[even] % 2 == 0:
+      even += 1
+    else:
+      A[even], A[odd] = A[odd], A[even]
+      odd -= 1
+  return A
+  
