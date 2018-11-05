5ab153eec3407567e7c961647435719b23e866ed
sh0sh0 <xue.yun.qi@gmail.com>
2018-11-05 14:12:18 -0800

    Update Even number first.py

--- Even number first.py
+++ Even number first.py
@@ -1,3 +1,4 @@
+## from EPI book, chapter 5
 ## given a list of integers, return the list with even numbers appear first
 ## you can create a new list, which will require extra space
 ## you can also using two pointers, one for even (starts from the beginning), and one for odd (starts from the end), to 
